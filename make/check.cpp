ae4bad1578d00dc112ea4beaf059822b10a8bb60
XY_cpp <1367595063@qq.com>
2021-02-09 16:32:50 +0800

    modify

---
+++ make/check.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool flag[10000];
+string s[10000];
+int n;
+int main()
+{
+    freopen("source","r",stdin);
+    string t;
+    while(getline(cin,t))
+    {
+        flag[++n]=1;
+        if(t.find("error")!=t.npos)
+            flag[n]=0;
+    }
+    fclose(stdin);
+    cin.clear();
+
+    freopen("server.yaml","r",stdin);
+        for(int i=1;i<=n;i++)
+            getline(cin,s[i]);
+    fclose(stdin);
+
+    freopen("server.yaml","w",stdout);
+        for(int i=1;i<=n;i++)
+            if(flag[i])
+                cout<<s[i]<<endl;
+    fclose(stdout);
+    return 0;
+}
